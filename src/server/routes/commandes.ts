323df6e2a418e2e2b999ad5276c4e39d50190b6b
Tenebrosful <hugobernard39@gmail.com>
2022-01-11 17:57:30 +0100

    Remove "" and add extra-info

--- src/server/routes/commandes.ts
+++ src/server/routes/commandes.ts
@@ -3,22 +3,24 @@ const commandes = express.Router();
 
 const persistDataCommandes = [
     {
-        "date_commande": "2022-01-05 12:00:23",
-        "id": "AuTR4-65ZTY",
-        "mail_client": "jan.neymar@yaboo.fr",
-        "montant": 25.95
+        date_commande: "2022-01-05 12:00:23",
+        id: "AuTR4-65ZTY",
+        mail_client: "jan.neymar@yaboo.fr",
+        montant: 25.95
     },
     {
-        "date_commande": "2022-01-06 16:05:47",
-        "id": "657GT-I8G443",
-        "mail_client": "jan.neplin@gmal.fr",
-        "montant": 42.95
+        date_commande: "2022-01-06 16:05:47",
+        id: "657GT-I8G443",
+        mail_client: "jan.neplin@gmal.fr",
+        montant: 42.95
     },
     {
-        "date_commande": "2022-01-07 17:36:45",
-        "id": "K9J67-4D6F5",
-        "mail_client": "claude.francois@grorange.fr",
-        "montant": 14.95
+        date_commande: "2022-01-07 17:36:45",
+        date_livraison: "2022-01-08 12:30",
+        id: "K9J67-4D6F5",
+        mail_client: "claude.francois@grorange.fr",
+        montant: 14.95,
+        nom_client: "claude francois"
     },
 ];
 
