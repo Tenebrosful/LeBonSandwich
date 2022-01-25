bd63d78209c8ddc218d5194276080d3164d70b45
Tenebrosful <hugobernard39@gmail.com>
2022-01-25 17:30:00 +0100

    Update database.ts

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,8 @@
-import { Sequelize, Dialect } from "sequelize";
 import * as dotenv from "dotenv";
+import { Dialect } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
+import { Client } from "./models/Client";
+import { Commande } from "./models/Commande";
 
 /**
  * Setup
@@ -10,15 +13,17 @@ dotenv.config({ path: "config/bdd.env" });
 let BDDInstance: Sequelize;
 
 export function initBDD() {
-  const instance = new Sequelize(
-    process.env.BDD_DATABASE || "",
-    process.env.BDD_USERNAME || "",
-    process.env.BDD_PASSWORD || "",
-    {
-      dialect: process.env.BDD_DRIVER as Dialect || "mariadb",
-      host: process.env.BDD_HOSTNAME || ""
-    }
-  );
+  console.log(process.env.MYSQL_DATABASE, process.env.BDD_DRIVER, process.env.MYSQL_HOST, process.env.MYSQL_PASSWORD, process.env.MYSQL_USER);
+
+  const instance = new Sequelize({
+    database: process.env.MYSQL_DATABASE,
+    dialect: process.env.BDD_DRIVER as Dialect,
+    host: process.env.MYSQL_HOST,
+    models: [Client, Commande],
+    password: process.env.MYSQL_PASSWORD,
+    port: 3306,
+    username: process.env.MYSQL_USER,
+  });
 
   try {
     instance.authenticate();
