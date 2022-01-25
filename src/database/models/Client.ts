29a407fb0b8761c0cb19db11f7a8c230e8da74db
Tenebrosful <hugobernard39@gmail.com>
2022-01-25 14:04:19 +0100

    Finalize Client model

--- src/database/models/Client.ts
+++ src/database/models/Client.ts
@@ -1,20 +1,29 @@
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { Column, CreatedAt, DataType, IsEmail, Model, NotNull, PrimaryKey, Table, UpdatedAt } from "sequelize-typescript";
 
 @Table
 export class Client extends Model {
-  @Column({type: DataType.STRING(128)})
+  @PrimaryKey
+  @Column({ autoIncrement: true, type: DataType.INTEGER })
+  id: number;
+
+  @NotNull
+  @Column({ type: DataType.STRING(128) })
   nom_client: string;
-  @Column({type: DataType.STRING(256)})
+
+  @NotNull
+  @IsEmail
+  @Column({ type: DataType.STRING(256) })
   mail_client: string;
-  @Column({type: DataType.STRING(256)})
+
+  @Column({ type: DataType.STRING(256) })
   passwd: string;
-  
-  
+
+  @Column({ type: DataType.DECIMAL(8, 2) })
   cumul_achats: number;
-  
-  
+
+  @CreatedAt
   created_at: Date;
-  
-  
+
+  @UpdatedAt
   updated_at: Date;
 }
