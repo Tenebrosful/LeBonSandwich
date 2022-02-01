ddc64b678b74ba05051a0f7bee0bce3f7f396b49
Geoffrey <geoffrey.poraykostexp7@gmail.com>
2022-02-01 15:46:58 +0100

    Update database.ts

--- src/database/database.ts
+++ src/database/database.ts
@@ -15,14 +15,35 @@ let BDDInstance: Sequelize;
 export function initBDD() {
   console.log(process.env.MYSQL_DATABASE, process.env.BDD_DRIVER, process.env.MYSQL_HOST, process.env.MYSQL_PASSWORD, process.env.MYSQL_USER);
 
+  // const instance = new Sequelize({
+  //   database: process.env.MYSQL_DATABASE,
+  //   dialect: process.env.BDD_DRIVER as Dialect,
+  //   host: process.env.MYSQL_HOST,
+  //   models: [Client, Commande],
+  //   password: process.env.MYSQL_PASSWORD,
+  //   port: 3306,
+  //   username: process.env.MYSQL_USER,
+  // });
+
+  /*
+MYSQL_HOST=LeBonSandwich_db
+MYSQL_ROOT_PASSWORD=a_password
+MYSQL_DATABASE=iut_db
+MYSQL_USER=iut_db
+MYSQL_PASSWORD=b_password
+MYSQL_PORT=3306
+
+BDD_DRIVER=mariadb
+  */
+
   const instance = new Sequelize({
-    database: process.env.MYSQL_DATABASE,
-    dialect: process.env.BDD_DRIVER as Dialect,
-    host: process.env.MYSQL_HOST,
+    database: "iut_db",
+    dialect: "mariadb",
+    host: "LeBonSandwich_db",
     models: [Client, Commande],
-    password: process.env.MYSQL_PASSWORD,
+    password: "b_password",
     port: 3306,
-    username: process.env.MYSQL_USER,
+    username: "iut_db",
   });
 
   try {
