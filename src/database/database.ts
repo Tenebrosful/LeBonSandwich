ef9f996d21d8344043c4a927dbb95a0986d726ef
Geoffrey <geoffrey.poraykostexp7@gmail.com>
2022-03-01 14:13:16 +0100

    update commande

--- src/database/database.ts
+++ src/database/database.ts
@@ -12,42 +12,23 @@ dotenv.config({ path: "config/bdd.env" });
 
 let BDDInstance: Sequelize;
 
-export function initBDD() {
+export async function initBDD() {
   console.log(process.env.MYSQL_DATABASE, process.env.BDD_DRIVER, process.env.MYSQL_HOST, process.env.MYSQL_PASSWORD, process.env.MYSQL_USER);
 
-  // const instance = new Sequelize({
-  //   database: process.env.MYSQL_DATABASE,
-  //   dialect: process.env.BDD_DRIVER as Dialect,
-  //   host: process.env.MYSQL_HOST,
-  //   models: [Client, Commande],
-  //   password: process.env.MYSQL_PASSWORD,
-  //   port: 3306,
-  //   username: process.env.MYSQL_USER,
-  // });
+const instance = new Sequelize({
+  database: process.env.MYSQL_DATABASE,
+  dialect: process.env.BDD_DRIVER as Dialect,
+  host: process.env.MYSQL_HOST,
+  models: [Client, Commande],
+  password: process.env.MYSQL_PASSWORD,
+  port: 3306,
+  username: process.env.MYSQL_USER,
+});
 
-  /*
-MYSQL_HOST=LeBonSandwich_db
-MYSQL_ROOT_PASSWORD=a_password
-MYSQL_DATABASE=iut_db
-MYSQL_USER=iut_db
-MYSQL_PASSWORD=b_password
-MYSQL_PORT=3306
-
-BDD_DRIVER=mariadb
-  */
-
-  const instance = new Sequelize({
-    database: "iut_db",
-    dialect: "mariadb",
-    host: "LeBonSandwich_db",
-    models: [Client, Commande],
-    password: "b_password",
-    port: 3306,
-    username: "iut_db",
-  });
 
   try {
-    instance.authenticate();
+    await instance.authenticate();
+    console.log('Connection has been established successfully.');
     BDDInstance = instance;
     return instance;
   }
--- src/server/routes/commandes.ts
+++ src/server/routes/commandes.ts
@@ -3,6 +3,8 @@ import { Commande } from "../../database/models/Commande";
 import error405 from "../errors/error405";
 const commandes = express.Router();
 
+commandes.use(restify.CORS());
+
 commandes.get("/", async (req, res, next) => {
   try {
     const allCommande = await Commande.findAll();
@@ -11,6 +13,15 @@ commandes.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
+commandes.put("/:id", async (req, res, next) => {
+  try{
+    const newOrUpdatedCommande = await Commande.update({ ...req.body }, { where: { id: req.params.id } });
+    res.status(204).send();
+  }catch(error){
+    next(error);
+  }
+})
 
 commandes.use("/", error405);
 
