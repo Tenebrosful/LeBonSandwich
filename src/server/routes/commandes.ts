f47d220a479d76c1e7b3a07a61e4af90e935fe0d
Geoffrey <geoffrey.poraykostexp7@gmail.com>
2022-03-01 15:50:22 +0100

    Update commandes.ts

--- src/server/routes/commandes.ts
+++ src/server/routes/commandes.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import { DatabaseError } from "sequelize";
 import { Commande } from "../../database/models/Commande";
 import error405 from "../errors/error405";
 const commandes = express.Router();
@@ -7,6 +8,15 @@ commandes.get("/", async (req, res, next) => {
   try {
     const allCommande = await Commande.findAll();
     res.status(200).json(allCommande);
+  } catch (error) {
+    next(error);
+  }
+});
+
+commandes.get("/:id", async (req, res, next) => {
+  try {
+    const commande = await Commande.findAll({where: {id: req.params.id}});
+    res.status(200).json(commande);
   } catch (error) {
     next(error);
   }
@@ -21,7 +31,8 @@ commandes.put("/:id", async (req, res, next) => {
 
   try {
     const newOrUpdatedCommande = await Commande.upsert({ id: req.params.id, ...commandFields });
-
+    console.log(newOrUpdatedCommande);
+    
     if (newOrUpdatedCommande[1])
       res.status(201).json(newOrUpdatedCommande[0].toJSON());
     else
@@ -31,6 +42,7 @@ commandes.put("/:id", async (req, res, next) => {
     if (error instanceof DatabaseError && error.original.code === "ER_NO_DEFAULT_FOR_FIELD") {
       res.status(422).json({
         code: 422,
+        // @ts-ignore
         message: `Command with id ${req.params.id} doesn't exist and required fields are missing in the request body${process.env.NODE_ENV === "dev" ? " [[[[ " + error.original.text + " ]]]]" : ""}`
       });
       return;
