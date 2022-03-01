2abddc1dbf72d3b9c3b611ec337f0c79b086effa
Tenebrosful <hugobernard39@gmail.com>
2022-03-01 16:08:52 +0100

    Re add import dialect

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv";
 import { Sequelize } from "sequelize-typescript";
+import { Dialect } from "sequelize";
 import { Client } from "./models/Client";
 import { Commande } from "./models/Commande";
 
