e9a531e55700b49d65532f1ad2160e50d453f029
Tenebrosful <hugobernard39@gmail.com>
2022-03-01 14:34:09 +0100

    Update put commandes

--- src/server/routes/commandes.ts
+++ src/server/routes/commandes.ts
@@ -13,13 +13,32 @@ commandes.get("/", async (req, res, next) => {
 });
 
 commandes.put("/:id", async (req, res, next) => {
-  try{
-    const newOrUpdatedCommande = await Commande.update({ ...req.body }, { where: { id: req.params.id } });
-    res.status(204).send();
-  }catch(error){
+  const commandFields = {
+    livraison: req.body.livraison,
+    mail: req.body.mail,
+    nom: req.body.nom
+  };
+
+  try {
+    const newOrUpdatedCommande = await Commande.upsert({ id: req.params.id, ...commandFields });
+
+    if (newOrUpdatedCommande[1])
+      res.status(201).json(newOrUpdatedCommande[0].toJSON());
+    else
+      res.status(204).send();
+  } catch (error) {
+    // @ts-ignore
+    if (error instanceof DatabaseError && error.original.code === "ER_NO_DEFAULT_FOR_FIELD") {
+      res.status(422).json({
+        code: 422,
+        message: `Command with id ${req.params.id} doesn't exist and required fields are missing in the request body${process.env.NODE_ENV === "dev" ? " [[[[ " + error.original.text + " ]]]]" : ""}`
+      });
+      return;
+    }
+
     next(error);
   }
-})
+});
 
 commandes.use("/", error405);
 
