79c2e8eb1e34e7115ac669ac951cc2834760141f
Tenebrosful <hugobernard39@gmail.com>
2022-03-04 12:57:28 +0100

    Create handleDataValidation.ts

---
+++ src/server/middleware/handleDataValidation.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { ObjectSchema } from "joi";
+import { error422Validation } from "../errors/error422";
+
+export default function handleDataValidation(validationSchema: ObjectSchema, data: Object, req: Request, res: Response, allRequired = false) {
+  const { error: validationError } = validationSchema.validate(data, {presence: (allRequired ? "required" : "optional")});
+
+  if (validationError) {
+    error422Validation(validationError, req, res);
+    return false;
+  }
+
+  return true;
+}
