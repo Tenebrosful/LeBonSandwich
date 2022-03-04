02a773864311b322495536a17b0fa4dba0dd2654
Tenebrosful <hugobernard39@gmail.com>
2022-03-04 11:13:01 +0100

    Rename to handleToken

--- src/server/middleware/handleToken.ts
+++ src/server/middleware/handleToken.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import error401 from "../errors/error401";
 
-export default function handleTokenMiddleware(req: Request, res: Response, next: NextFunction) {
+export default function handleToken(req: Request, res: Response, next: NextFunction) {
   res.locals.token = req.query.token || req.headers["X-lbs-token"];
 
   if (res.locals.token) next();
