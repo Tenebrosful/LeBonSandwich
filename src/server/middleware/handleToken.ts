e2879f21690c2f8e512f52f318000aa8161ceb05
Geoffrey <geoffrey.poraykostexp7@gmail.com>
2022-03-04 11:45:07 +0100

    Update handleToken.ts

--- src/server/middleware/handleToken.ts
+++ src/server/middleware/handleToken.ts
@@ -4,6 +4,7 @@ import error401 from "../errors/error401";
 export default function handleToken(req: Request, res: Response, next: NextFunction) {
   res.locals.token = req.query.token || req.headers["x-lbs-token"];
 
+  if (res.locals.token) { next(); return; };
 
   error401(req, res);
 }
