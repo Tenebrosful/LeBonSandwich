8a2f04d3441ae41d4e2a7cf0d9548125f3477632
Geoffrey <geoffrey.poraykostexp7@gmail.com>
2022-03-08 15:12:01 +0100

    séparation des services

---
+++ database/config/README.md
@@ -0,0 +1,7 @@
+# Setup .env files
+
+## `bdd.env`
+
+```env
+BDD_URL=
+```
--- prise-commande/src/server/routes/commandes.ts
+++ prise-commande/src/server/routes/commandes.ts
@@ -1,15 +1,15 @@
 import * as express from "express";
-import { Commande } from "../../database/models/Commande";
-import { Item } from "../../database/models/Item";
+import { Commande } from "../../../../database/models/Commande";
+import { Item } from "../../../../database/models/Item";
 import error405 from "../errors/error405";
 import { error422DatabaseUpdate } from "../errors/error422";
 import handleToken from "../middleware/handleToken";
 import * as jwt from "jsonwebtoken";
 import { ResponseAllCommandes, ResponseCollection, ResponseCommande, ResponseItem, ResponseType } from "../types/ResponseTypes";
-import CommandeSchema from "../../database/validateSchema/CommandeSchema";
+import CommandeSchema from "../../../../database/validateSchema/CommandeSchema";
 import handleDataValidation from "../middleware/handleDataValidation";
 import { RequestItem } from "../types/RequestTypes";
-import CommandeItemSchema from "../../database/validateSchema/CommandeItemSchema";
+import CommandeItemSchema from "../../../../database/validateSchema/CommandeItemSchema";
 const commandes = express.Router();
 
 commandes.get("/", async (req, res, next) => {
