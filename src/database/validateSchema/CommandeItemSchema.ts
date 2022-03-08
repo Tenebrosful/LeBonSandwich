688f2bce4847bd988b005f980e5d9ca4c1e1aed6
Tenebrosful <hugobernard39@gmail.com>
2022-03-08 14:51:20 +0100

    remove uri validation

--- src/database/validateSchema/CommandeItemSchema.ts
+++ src/database/validateSchema/CommandeItemSchema.ts
@@ -4,7 +4,7 @@ const CommandeItemSchema = Joi.object({
   libelle: Joi.string().alphanum(),
   quantite: Joi.number().positive().greater(0).integer(),
   tarif: Joi.number().positive().precision(2),
-  uri: Joi.string().uri(),
+  uri: Joi.string(),
 });
 
 export default CommandeItemSchema;
